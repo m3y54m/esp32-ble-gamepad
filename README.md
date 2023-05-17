a812df4e03dd45592468149d7894a5dfa6effc6e
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2023-05-17 17:20:14 +0330

    Single button test

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # DIY Gamepad using ESP32
+
+## Resources
+
+- [Bluetooth LE Gamepad library for the ESP32](https://github.com/lemmingDev/ESP32-BLE-Gamepad)
+- [DIY gamepad using ESP32 and controlled over BLE](https://github.com/palsayantan/BLE-Game-Controller)
+- [ESP32 BLE GamePad](https://www.instructables.com/ESP32-BLE-GamePad/)
+- [JOYCON Button and Analog Stick Board](https://www.instructables.com/JOYCON-Button-and-Analog-Stick-Board/)
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
+/*
+ * A simple sketch that maps a single pin on the ESP32 to a single button on the controller
+ */
+
 #include <Arduino.h>
+#include <BleGamepad.h> // https://github.com/lemmingDev/ESP32-BLE-Gamepad
 
-// put function declarations here:
-int myFunction(int, int);
+#define BUTTONPIN 35 // Pin button is attached to
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+BleGamepad bleGamepad;
+
+int previousButton1State = HIGH;
+
+void setup()
+{
+    pinMode(BUTTONPIN, INPUT_PULLUP);
+    bleGamepad.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+    if (bleGamepad.isConnected())
+    {
+        int currentButton1State = digitalRead(BUTTONPIN);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+        if (currentButton1State != previousButton1State)
+        {
+            if (currentButton1State == LOW)
+            {
+                bleGamepad.press(BUTTON_1);
+            }
+            else
+            {
+                bleGamepad.release(BUTTON_1);
+            }
+        }
+        previousButton1State = currentButton1State;
+    }
 }
